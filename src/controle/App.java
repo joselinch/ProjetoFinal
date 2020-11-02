bbd01643058e72d07001f9bddbce216316e94d48
joselinch <51121516+joselinch@users.noreply.github.com>
2020-11-02 19:09:19 -0300

    Classe de controle App
    Criação da classe principal da aplicação com o método main.

--- src/controle/App.java
+++ src/controle/App.java
@@ -1,5 +1,48 @@
 package controle;
 
-public class App {
+import java.util.Scanner;
 
+import dao.RelatoriosDAO;
+import modelo.Atendimento;
+import modelo.Funcionalidades;
+import modelo.Paciente;
+import visao.TelaAtendimento;
+import visao.TelaCadastro;
+import visao.TelaConsulta;
+import visao.TelaLogin;
+import visao.TelaPrincipal;
+import visao.TelaRealizarConsulta;
+import visao.TelaTriagem;
+
+
+public class App {
+	
+public static void main(String[] args) {
+
+		//instancia os objetos das telas, das filas, listas e funcionalidades.
+		TelaPrincipal tp = new TelaPrincipal();
+		TelaCadastro tc = new TelaCadastro();
+		TelaTriagem tt = new TelaTriagem();
+		TelaConsulta tco = new TelaConsulta();	
+		TelaLogin tl = new TelaLogin();
+		TelaAtendimento ta = new TelaAtendimento();
+		TelaRealizarConsulta trc = new TelaRealizarConsulta();
+		Funcionalidades f = new Funcionalidades();
+		RelatoriosDAO r = new RelatoriosDAO();
+		Paciente p = new Paciente();
+		Atendimento a = new Atendimento(); 
+		
+		//cria os construtores de cada tela com seus respectivos objetos
+		ControleTelaRealizarConsulta controletrc = new ControleTelaRealizarConsulta(tp, trc, f);
+		ControleTelaTriagem controlett = new ControleTelaTriagem(f, tp, tt);
+		ControleTelaAtendimento controleta = new ControleTelaAtendimento(a, ta, tp, f, p);
+		ControleTelaCadastro controletc = new ControleTelaCadastro(tp, tc, f, p);
+		ControleTelaConsulta controletco = new ControleTelaConsulta(tp, tco, f, p);
+		ControleTelaPrincipal controletp = new ControleTelaPrincipal(tp, tc, tco, tt, ta, trc, r);
+		ControleTelaLogin controletl = new ControleTelaLogin(tp, tl);
+		
+		//deixa a tela de login visivel
+		tl.setVisible(true);
+		
+}
 }
